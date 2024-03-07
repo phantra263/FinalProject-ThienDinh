74d2a7dd4dc6f76436d86b9b03899ec644c5d219
ESUHAI\trapc <phantra2212@gmail.com>
2024-03-07 14:16:15 +0700

    fix login

--- FinalProject-Update1/assets/js/LogInAndSignUp.js
+++ FinalProject-Update1/assets/js/LogInAndSignUp.js
@@ -1,3 +1,17 @@
+// kiểm tra user đã đăng nhập hay chưa
+let loginHtml = document.getElementById("control-login");
+let currentUser = JSON.parse(localStorage.getItem("user")) || '';
+let logout = document.getElementById("logout");
+if (currentUser && currentUser.username) {
+    loginHtml.innerHTML = `${currentUser.username} / <i class="fa-solid fa-arrow-right-from-bracket" id="logout" style="cursor: pointer"></i>`;
+} else loginHtml.innerHTML = `<a href="#0" id="login">ĐĂNG NHẬP</a>`
+
+// logout
+document.getElementById("logout").addEventListener("click", function () {
+    localStorage.removeItem("user");
+    window.location.href = "login.html"
+})
+
 function signup(e) {
     event.preventDefault();
     var username = document.getElementById("username").value;
@@ -9,9 +23,9 @@ function signup(e) {
         password: password,
     };
     var json = JSON.stringify(user);
-    localStorage.setItem(username, json);
+    localStorage.setItem("user", json);
     alert("Đăng ký thành công! Mời bạn đăng nhập")
-    window.location.href="login.html"
+    window.location.href = "login.html"
 }
 
 function login(e) {
@@ -19,16 +33,16 @@ function login(e) {
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    var user = localStorage.getItem(username);
-    var data = JSON.parse(user);
-    console.log(user);
-    console.log(data);
-    if(user == null)  {
+    // var user = localStorage.getItem("user");
+    // var data = JSON.parse(user);
+    // console.log(user);
+    // console.log(data);
+    if (currentUser == null) {
         alert("Vui lòng nhập Email và mật khẩu đăng nhập")
-    } else if (email == data.email && 
-                password == data.password) {
+    } else if (email == currentUser.email &&
+        password == currentUser.password) {
         alert("Đăng nhập thành công")
-        window.location.href="index.html"
+        window.location.href = "index.html"
     } else {
         alert("Tài khoản hoặc mật khẩu không chính xác")
     }
